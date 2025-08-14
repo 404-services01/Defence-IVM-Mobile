64530c54b8f39abb10bfb45df5b46fbab02e2adc
404-services01 <404services1@gmail.com>
2025-08-13 18:08:37 -0700

    NonsoLatest

--- App.tsx
+++ App.tsx
@@ -1,27 +1,9 @@
 import { addScreenshotListener, usePreventScreenCapture } from 'expo-screen-capture';
 import WebViewMain from './Components/src/IntroScreen/WebViewMain';
-import * as Permisssions from "expo-permissions";
-import { useEffect } from 'react';
 
 
 export default function App() {
   usePreventScreenCapture();
-
-  // async function getCameraPermission(){
-  //   const [status]:any = await Permisssions.askAsync(
-  //     Permisssions.CAMERA_ROLL
-  //   )
-  //   if (status === "granted") 
-  //     {
-  //       addScreenshotListener(() => {
-  //         alert("You took a screenshot!")
-  //       })
-  //     }
-  // }
-
-  // useEffect(() => {
-  //   getCameraPermission()
-  // }, [])
 
   return <WebViewMain />;
 }
